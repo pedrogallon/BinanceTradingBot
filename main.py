d0fb653ba446517bf14eac94635053433093af22
pedrogallon <pedrogallonalves@hotmail.com>
2021-09-14 19:57:44 -0300

    prettify

--- main.py
+++ main.py
@@ -13,46 +13,33 @@ import json
 settingsFile = open('settings.json').read()
 settings = json.loads(settingsFile)
 
-#settings = json.loads(settingsFile)
-
-#print(settings.get("api_key"))
-
-# API: https://python-binance.readthedocs.io/en/latest/account.html
-
-#MEUS_ATIVOS = ['BTCUSDT','ETHUSDT','LTCUSDT','ADAUSDT','SOLUSDT']
-
-###
-# client = Client(api_key, api_secret)
 client = Client(settings.get("api_key"), settings.get("api_secret"))
-
 res = client.get_account()
-#print(res.get('balances'))
 
 print('-- ACCOUNT BALANCE --')
 for b in res.get('balances'):
-    if float(b.get('free'))> 0:
-        print(b.get('asset')+": "+b.get('free'))
+    if float(b.get('free')) > 0:
+        print(b.get('asset') + ": " + b.get('free'))
 
-''
 currentPrices = client.get_all_tickers()
 oldPrices = currentPrices
 
-def checkPrices():
+
+def check_prices():
     print('\n-- CHECKING PRICES --')
     global currentPrices
     global oldPrices
     currentPrices = client.get_all_tickers()
-    for current, old in zip(currentPrices,oldPrices) :
+    for current, old in zip(currentPrices, oldPrices):
         if current.get('symbol') in settings.get("tickers"):
             print(current.get('symbol'))
-            increasePercent = (float(current.get('price'))- float(old.get('price'))) / float(old.get('price')) * 100
-            print(increasePercent)
-            
-    
+            increase_percent = (float(current.get('price')) - float(old.get('price'))) / float(old.get('price')) * 100
+            print(increase_percent)
     oldPrices = currentPrices
-    
+
+
 schedule.clear()
-schedule.every(10).seconds.do(checkPrices)
+schedule.every(10).seconds.do(check_prices)
 while True:
     schedule.run_pending()
     time.sleep(1)
